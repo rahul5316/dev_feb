025534cc1e23b90298f1af856ae091a99ece64a7
Rahul <rahulkumarsinha5316@gmail.com>
2021-05-04 23:35:39 +0530

    Changed Background colors

---
+++ DOM/Jira_Ticket_Manager/Activity/script.js
@@ -0,0 +1,22 @@
+
+let allFilters = document.querySelectorAll(" .ticket-filter div"); // gives all the divs inside ticket-filter
+
+
+let filterCodes = {
+  
+  "pink":"#8e44ad",
+  "blue":"#3498db",
+  "black":"#2c3e50",
+  "green":"#2ecc71"
+}
+let ticketContainer = document.querySelector(".tickets-container");
+
+for(let i =0; i<allFilters.length;i++) {
+  allFilters[i].addEventListener("click",chooseFilter);
+}
+
+function chooseFilter(e){
+  let filter = e.target.classList[1];//gets whatever color was clicked
+  let filterCode = filterCodes[filter];//finds the code of that colour from above
+  ticketContainer.style.background = filterCode;//sets the background to this certain color
+}
